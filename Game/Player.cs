04473094c5f764a1c7cf700810831a92676d154f
KeiserKholod <38317130+KeiserKholod@users.noreply.github.com>
2020-04-25 21:12:40 +0500

    added basic classes

---
+++ Game/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameName
+{
+    class Player
+    {
+        public readonly String Name;
+        public readonly int Id;
+
+        public Player(String name, int id)
+        {
+            Id = id;
+            Name = name;
+        }
+        public Player(int id)
+        {
+            Id = id;
+            Name = "player"+Id;
+        }
+
+        public void MakeMove(Game game)
+        {
+            int x = 0;
+            int y = 0;
+            //закрашиваем магически выбранную клетку в Id игрока
+            game.Map[x, y] = Id;
+            //something
+        }
+    }
+}
