3cf27d30862b2e97bb9a74c5cd45839f7c324093
Titov Stepan <titovstepan@mail.ru>
2020-05-02 23:37:18 +0500

    init commit

---
+++ CZT/Core/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CZT.Core
+{
+    class Player
+    {
+        public readonly String Name;
+        public readonly int Id;
+
+        public Player(String name, int id)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Player(int id)
+        {
+            Id = id;
+            Name = "player" + Id;
+        }
+
+        public void MakeMove(Game game)
+        {
+            int x = 0;
+            int y = 0;
+            //закрашиваем магически выбранную клетку в Id игрока
+            game.Map[x, y] = Id;
+            //something
+        }
+    }
+}
